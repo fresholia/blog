0e2a17174ece477bf480172ff89209ac345769e8
Enes Akıllıok <enesakilliok17@gmail.com>
2022-04-11 11:32:48 +0300

    Update PageContents.js

--- src/exports/PageContents.js
+++ src/exports/PageContents.js
@@ -1,6 +1,6 @@
 module.exports = {
     'indexHeader': `Ben Enes Akıllıok.`,
-    'about': `Merhaba, küçük yaşlarımdan beri hobi olarak başladığım yazılım ve kodlamaya hala severek devam ediyorum. GitHub profilim üzerinde projelerimi açık kaynaklı şekilde paylaşıyorum. Hayat mottomun ortasında fazla çalışmak değil akıllı çalışmak var.`,
+    'about': `Merhaba, küçük yaşlarımdan beri hobi olarak başladığım kodlama ve tasarımı iş olarak sahiplenerek devam ediyorum.`,
 
     'langs': ['JavaScript', 'Lua', 'React', 'Next.js', 'PHP', 'REST', 'OOP Scripting'],
 
